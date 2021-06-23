2e24dffa4ea0cd6d831cd0b103475fd5a442520b
Zhenya <zhenya-bogdasheva@ya.ru>
2021-06-23 21:14:18 +0300

    sending_e-mail_context_processors

---
+++ geekshop/authapp/views.py
@@ -0,0 +1,96 @@
+import logging
+
+from django.conf import settings
+from django.contrib import auth
+from django.core.mail import send_mail
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
+from authapp.forms import ShopUserLoginForm, ShopUserRegisterForm, ShopUserEditForm
+from authapp.models import ShopUser
+
+
+def login(request):
+    redirect_to = request.GET.get('next', '')
+
+    login_form = ShopUserLoginForm(data=request.POST)
+    if request.method == 'POST' and login_form.is_valid():
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        redirect_to = request.POST.get('next')
+        user = auth.authenticate(username=username, password=password)
+        if user and user.is_active:
+            auth.login(request, user)
+            return HttpResponseRedirect(redirect_to or reverse('index'))
+    else:
+        form = ShopUserLoginForm()
+
+    context = {
+        'page_title': 'вход',
+        'login_form': login_form,
+        'redirect_to': redirect_to,
+    }
+    return render(request, 'authapp/login.html', context)
+
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('index'))
+
+
+def register(request):
+    if request.method == 'POST':
+        register_form = ShopUserRegisterForm(request.POST, request.FILES)
+
+        if register_form.is_valid():
+            user = register_form.save()
+            send_verify_link(user)
+            # if send_verify_link(user):
+            #     logging.debug('sent successful')
+            # else:
+            #     logging.debug('sent failed')
+            return HttpResponseRedirect(reverse('auth:login'))
+    else:
+        register_form = ShopUserRegisterForm()
+
+    context = {
+        'page_title': 'регистрация',
+        'register_form': register_form,
+    }
+
+    return render(request, 'authapp/register.html', context)
+
+
+def edit(request):
+    if request.method == 'POST':
+        edit_form = ShopUserEditForm(request.POST, request.FILES, instance=request.user)
+        if edit_form.is_valid():
+            edit_form.save()
+            return HttpResponseRedirect(reverse('auth:edit'))
+    else:
+        edit_form = ShopUserEditForm(instance=request.user)
+
+    context = {
+        'page_title': 'редактирование',
+        'edit_form': edit_form,
+    }
+
+    return render(request, 'authapp/edit.html', context)
+
+
+def send_verify_link(user):
+    verify_link = reverse('auth:verify', args=[user.email, user.activation_key])
+    subject = 'Account verify'
+    message = f'Your link for account activation: {settings.DOMAIN_NAME}{verify_link}'
+    return send_mail(subject, message, settings.EMAIL_HOST_USER, [user.email], fail_silently=False)
+
+
+def verify(request, email, key):
+    user = ShopUser.objects.filter(email=email).first()
+    if user and user.activation_key == key and not user.is_activation_key_expired():
+        user.is_active = True
+        user.activation_key = ''
+        user.activation_key_created = None
+        user.save()
+        auth.login(request, user)
+    return render(request, 'authapp/verify.html')
+
