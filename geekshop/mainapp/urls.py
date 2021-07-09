344a53e41707087a9101843edffc756b50ef8c84
Zhenya <zhenya-bogdasheva@ya.ru>
2021-07-09 19:48:45 +0300

    leftover_goods

--- geekshop/mainapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import category, products, product_page
-
-
-urlpatterns = [
-    path('', products, name='products'),
-    path('category/<int:pk>/', category, name='category'),
-    path('product/<int:pk>/', product_page, name='product_page'),
-]
